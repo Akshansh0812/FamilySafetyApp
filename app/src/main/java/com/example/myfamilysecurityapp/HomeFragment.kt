6393b9ba97165039547f23505519c5ab69be4f77
Akshansh0812 <akshanshanand2001@gmail.com>
2023-04-08 16:11:36 +0530

    Add Recycler View

--- app/src/main/java/com/example/myfamilysecurityapp/HomeFragment.kt
+++ app/src/main/java/com/example/myfamilysecurityapp/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeFragment : Fragment() {
 
@@ -19,6 +21,24 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val listMembers = listOf<MemberModel>(
+            MemberModel("Akshay"),
+            MemberModel("Tanu"),
+            MemberModel("Mummy"),
+            MemberModel("Papa"),
+            MemberModel("Aashish")
+        )
+        val adapter = MemberAdapter(listMembers)
+
+        val recycler = requireView().findViewById<RecyclerView>(R.id.recycler_member)
+        recycler.layoutManager = LinearLayoutManager(requireContext())
+        recycler.adapter = adapter
+    }
+
 
     companion object {
         @JvmStatic
---
+++ app/src/main/java/com/example/myfamilysecurityapp/MemberModel.kt
@@ -0,0 +1,5 @@
+package com.example.myfamilysecurityapp
+
+class MemberModel(
+    val name : String
+)
